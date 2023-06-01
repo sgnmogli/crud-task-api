36be9eac55c2cb1e57a7b71f82a73618f5775132
Satopanth <sgnmogli@gmail.com>
2023-06-02 00:43:30 +0530

    Added error message

--- database/mongoose.js
+++ database/mongoose.js
@@ -5,7 +5,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/taskmanagerdb')
     console.log("DB connected successfully")
 })
 .catch((error)=>{
-    console.log(error)
+    console.log("error occurred while DB Connection",error)
 });
 
 module.exports = mongoose;
